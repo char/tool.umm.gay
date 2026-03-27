27cc706c2ca0046183ec359a0ed04fd9398dc03d
Charlotte Som <charlotte@som.codes>
2026-03-27 11:10:43 +0000

    enable cors at apex

--- ngx.ts
+++ ngx.ts
@@ -7,6 +7,7 @@ export const config = ngx(undefined, [
     ngx.letsEncrypt("tool.umm.gay"),
     ngx("location /", [
       "charset utf-8",
+      "add_header Access-Control-Allow-Origin *",
       "root /srv/www/tool.umm.gay/hub/public",
     ]),
   ]),
